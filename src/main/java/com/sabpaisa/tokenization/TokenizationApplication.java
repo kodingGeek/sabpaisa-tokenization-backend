99d4ee16ad5a793659b375ad00d6b4986b61ab09
manish-prajapati <manish.prajapati@sabpaisa.in>
2025-10-13 12:07:19 +0000

    Configure application for WAR deployment to Tomcat

--- src/main/java/com/sabpaisa/tokenization/TokenizationApplication.java
+++ src/main/java/com/sabpaisa/tokenization/TokenizationApplication.java
@@ -2,9 +2,16 @@ package com.sabpaisa.tokenization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TokenizationApplication {
+public class TokenizationApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TokenizationApplication.class);
+    }
     
     public static void main(String[] args) {
         SpringApplication.run(TokenizationApplication.class, args);
